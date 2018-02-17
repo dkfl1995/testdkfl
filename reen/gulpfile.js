a2493ab21286a07ac5b68226fa9a09332fbb2ced
dkfl1995 <vlaio.95@gmail.com>
2018-02-17 21:45:34 +0200

    creating header and tophead

--- reen/gulpfile.js
+++ reen/gulpfile.js
@@ -2,14 +2,17 @@
 
 var gulp = require("gulp");
 var prefix = require("gulp-autoprefixer");
+var sourcemaps = require('gulp-sourcemaps');
 var sass = require("gulp-sass");
 
-var path = "./scss/style.scss";
-var dist = "./css/style.css"; 
+var path = "./scss/**/style.scss";
+var dist = "./css/"; 
 
 gulp.task('watch', function(){
     return gulp.src(path)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(prefix())
         .pipe(gulp.dest(dist));
 });
