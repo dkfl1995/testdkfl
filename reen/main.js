187f05cc2a482642c2b30c6751ff7bc99eafc421
dkfl1995 <vlaio.95@gmail.com>
2018-03-01 15:32:28 +0200

    f

--- reen/main.js
+++ reen/main.js
@@ -2,8 +2,30 @@ $(document).ready(
     $('.slicky').slick({
         dots: true,
         slidesToShow: 4,
-        responsive: [{
-
-        }]
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        responsive: [
+            {
+                breakpoint:1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false
+                }
+            },
+            {
+                breakpooint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false
+                }
+            }
+        ]
     })
 );
