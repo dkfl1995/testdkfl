ee549722b05718b345e403577eb06e7537b5a0f7
dkfl1995 <vlaio.95@gmail.com>
2018-03-12 13:18:11 +0200

    animation for input-search

--- reen/main.js
+++ reen/main.js
@@ -1,36 +1,79 @@
+
+    // var prev = document.createElement('button');
+    // prev.className = 'prev';
+    // prev.innerHTML = 'prev';
+    // var next = document.createElement('button');
+    // next.innerHTML = 'next';
+    // next.className = 'next';
+    // this.console.log(prev);
+
 $(document).ready(
     $('.slicky').slick({
         arrows: false,
-        dots: true,
-        slidesToShow: 4,
-        slidesToScroll: 2,
         infinite: true,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        //prevArrow: '<li><button type="button" class="slick-prev">Previous</button></li>',
-        //nextArrow: '<li><button type="button" class="slick-next">Next</button></li>',
         speed: 1000,
-        responsive: [
-            {
-                breakpoint:1024,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    dots: false
-                }
-            },
-            {
-                breakpooint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    dots: false
-                }
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+
             }
-        ]
+        }, {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                
+                infinite: true,
+
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                
+                infinite: true,
+                
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }]
     })
 );
+
+var searchFor = document.getElementsByName('searchFor')[0];
+console.log(searchFor);
+
+
+document.getElementsByName('submitSearch')[0].addEventListener('click', function (e){
+    searchFor.classList.add('searchFor-anim');
+});
+
+document.getElementsByName('submitSearch')[0].addEventListener('click', function (e){
+    searchFor.classList.remove('searchFor-anim');
+});
+
+$('.prev').on('click', function(){
+    $('.slicky').slick('slickPrev');
+});
+$('.next').on('click', function(){
+    $('.slicky').slick('slickNext');
+});
+
+
+
+
+
